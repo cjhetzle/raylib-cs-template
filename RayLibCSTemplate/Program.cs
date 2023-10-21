d593deb98ccc1419b752a3285870f68b6e3a2bc5
Cameron Hetzler <cjhetz@gmail.com>
2023-10-20 22:53:17 -0400

    init

---
+++ RayLibCSTemplate/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Raylib_cs;
+
+namespace HelloWorld
+{
+    static class Program
+    {
+        public static void Main()
+        {
+            Raylib.InitWindow(800, 480, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
